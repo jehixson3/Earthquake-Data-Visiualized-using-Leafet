34f83d2f5127d2a05c00f62ee5a80af9ad3db77a
John Hixson <johnhixson2020@u.northwestern.edu>
2020-01-02 14:19:22 -0600

    Still working on it

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,38 +1,28 @@
 // Store Endpoint for URL
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson" 
-
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
-
 function createFeatures(earthquakeData) {
-
-  // Create array to replace markers with circles
-  var cicleArray = new Array();
-
-  // Loop through the data
-  
   // Define a function to run once for each feature in features array
   // Give each pop up to describe place ands magnitude
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place + 
     "</h3> <hr> <h3>Magnitude: " + feature.properties.mag.toFixed(2) + "</h3>");
   }
-
   // Create Geojson layer containing features array on the earthquaker object
   // Run the onEachFeature funcetion once for each peice of information in the array
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
-
   // Sending Earthquake layer to CreatreMap function
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
-  // Define layers
+  // Define streetmap and satellite layer
   var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -40,21 +30,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  });
   var outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.outdoors",
     accessToken: API_KEY
   });
+
   var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
+  id: "mapbox.dark",
   id: "mapbox.satellite",
   accessToken: API_KEY
 });
@@ -62,8 +48,7 @@ function createMap(earthquakes) {
   var baseMaps = {
    "Satellite": satellitemap,
    "Grayscale": lightmap,
-   "Outdoors": outdoormap,
-   "Streets": streetmap
+   "Outdoors": outdoormap
   };
 
   // Create overlay object to hold our overlay layer
@@ -71,13 +56,14 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
   // Create our map, giving it the base maps and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [satellitemap, lightmap, outdoormap, streetmap, earthquakes]
+    layers: [satellitemap, earthquakes]
   });
 
   // Create a layer control
@@ -87,24 +73,5 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
-// Color function
-function chooseColor(magnitude) {
-  if (magnitude < 1) {
-    return "green"
-  }
-  else if (magnitude < 2) {
-    return "lightgreen"
-  }
-  else if (magnitude < 3) {
-    return "yellow"
-  }
-  else if (magnitude < 4) {
-    return "orange"
-  }
-  else if (magnitude < 5 ) {
-    return "orangered"
-  }
-  else if (magnitude > 5) {
-    return "red"
-  }
-  };
+
+ 
